4708a6156108bae2c574fca583fc3cc907bcdd2b
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-06-29 11:55:23 +0300

    7/1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,8 +6,8 @@ console.log(galleryItems);
 const galleryEl = document.querySelector(".gallery");
 
 const makeGallery = galleryItems.map((e) => {
-  const galarryItem = document.createElement(`div`);
-  galarryItem.classList.add(`gallery__item`);
+  // const galarryItem = document.createElement(`div`);
+  // galarryItem.classList.add(`gallery__item`);
   const makeItemLink = () => {
     const galleryItemLink = document.createElement(`a`);
     galleryItemLink.classList.add(`gallery__link`);
@@ -15,27 +15,41 @@ const makeGallery = galleryItems.map((e) => {
       const imgItem = document.createElement(`img`);
       imgItem.classList.add(`gallery__image`);
       imgItem.src = e.preview;
-      imgItem.dataSource = e.original;
+      // imgItem.dataSource = e.original;
       imgItem.alt = e.description;
       return imgItem;
     };
     galleryItemLink.append(makeImg());
     return galleryItemLink;
   };
-  galarryItem.append(makeItemLink());
-  return galarryItem;
+  galleryEl.append(makeItemLink());
+  // return galarryItem;
 });
 galleryEl.append(...makeGallery);
 
-galleryEl.addEventListener(`click`, (e) => {
-  gallery.on("show.simplelightbox");
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionPosition: "bottom",
+  captionData: "alt",
+  navtext: ["<~", "~>"],
 });
-let gallery = new SimpleLightbox(".gallery a");
-gallery.on("show.simplelightbox", function () {
-  console.log(`asdsaa`);
 
-  // Do something…
-});
+{
+  /* <a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a>;
+ */
+}
+
+// galleryEl.addEventListener(`click`, (e) => {
+//   gallery.on("show.simplelightbox");
+// });
+// let gallery = new SimpleLightbox(".gallery a");
+// gallery.on("show.simplelightbox", function () {
+//   console.log(`asdsaa`);
+
+//   // Do something…
+// });
 
 // **Example**
 // ```javascript
